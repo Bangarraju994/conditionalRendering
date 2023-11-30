16ebbd5eafc1789252aec5a75cbade3d02045bde
Raju994 <bangarraju994@gmail.com>
2023-11-30 10:35:06 +0530

    username and password inputs are added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import LoginPage from "./Components/Loginpage";
 
 const App = () => (
 
-  <div>
+  <div >
     <h1 > Hello, User </h1> 
+    <div >
+      <input className="container" type="text" />
+    </div>
+    <div>
+      <input className="container" type="password" />
+    </div>
     <div> 
       <LoginPage />
     </div>
